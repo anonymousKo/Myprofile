73e203523f8af08762f25b079e170b9e63402482
koresuki <1789151097@qq.com>
2021-10-08 10:18:11 +0800

    1

--- win10/win10_initial.psm1
+++ win10/win10_initial.psm1
@@ -1,32 +1,50 @@
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -Verb RunAs
-	Exit
-}
 	
  #Disables Web Search in Start Menu
-    Write-Output "Disabling Bing Search in Start Menu"
-    $WebSearch = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
-    Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" BingSearchEnabled -Value 0 
-    If (!(Test-Path $WebSearch)) {
-        New-Item $WebSearch
-    }
-    Set-ItemProperty $WebSearch DisableWebSearch -Value 1 
+Function Disable-WebSearch {
+	Write-Output "Disabling Bing Search in Start Menu..."
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "CortanaConsent" -Type DWord -Value 0
+	If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search")) {
+		New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "DisableWebSearch" -Type DWord -Value 1
+}
+ # Enable Web Search in Start Menu
+Function Enable-WebSearch {
+	Write-Output "Enabling Bing Search in Start Menu..."
+	Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -ErrorAction SilentlyContinue
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "CortanaConsent" -Type DWord -Value 1
+	Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "DisableWebSearch" -ErrorAction SilentlyContinue
+}
 	
 #Disables scheduled tasks that are considered unnecessary 
+function Disable-Xbox{
     Write-Output "Disabling scheduled tasks"
     Get-ScheduledTask  XblGameSaveTaskLogon | Disable-ScheduledTask
     Get-ScheduledTask  XblGameSaveTask | Disable-ScheduledTask
+}
+function Enable-Xbox{
+    Write-Output "Enabling scheduled tasks"
+    Get-ScheduledTask  XblGameSaveTaskLogon | Enable-ScheduledTask
+    Get-ScheduledTask  XblGameSaveTask | Enable-ScheduledTask
+}
+function Disable-DataCollect{
 	#data collect
     Get-ScheduledTask  Consolidator | Disable-ScheduledTask
     Get-ScheduledTask  UsbCeip | Disable-ScheduledTask
     Get-ScheduledTask  DmClient | Disable-ScheduledTask
     Get-ScheduledTask  DmClientOnScenarioDownload | Disable-ScheduledTask
+}
 	
-#defender
+function Disable-Defender{
 	Set-MpPreference -DisableRealtimeMonitoring $true
-	
+}
+function Enable-Defender{
+	Set-MpPreference -DisableRealtimeMonitoring $false
+}
 	
 #remove apps
+function Remove-Apps{
 	$Bloatware = @(
 
         #Unnecessary Windows 10 AppX Apps
@@ -40,7 +58,6 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
         "Microsoft.NetworkSpeedTest"
         "Microsoft.News"
         "Microsoft.Office.Lens"
-        "Microsoft.Office.OneNote"
         "Microsoft.Office.Sway"
         "Microsoft.OneConnect"
         "Microsoft.People"
@@ -100,4 +117,24 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
         #Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like $Bloat | Remove-AppxProvisionedPackage -Online
         Write-Output "Trying to remove $Bloat."
     }
-pause
+}
+	
+# Disable Activity History feed in Task View
+# Note: The checkbox "Store my activity history on this device" ("Let Windows collect my activities from this PC" on older versions) remains checked even when the function is disabled
+Function Disable-ActivityHistory {
+	Write-Output "Disabling Activity History..."
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "EnableActivityFeed" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "PublishUserActivities" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "UploadUserActivities" -Type DWord -Value 0
+}
+
+# Enable Activity History feed in Task View
+Function Enable-ActivityHistory {
+	Write-Output "Enabling Activity History..."
+	Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "EnableActivityFeed" -ErrorAction SilentlyContinue
+	Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "PublishUserActivities" -ErrorAction SilentlyContinue
+	Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "UploadUserActivities" -ErrorAction SilentlyContinue
+}
+
+# Export functions
+Export-ModuleMember -Function *
